80d77042b3a7877e68121e92fe3a8dca6e8e6917
Веолетта <veoletta@MacBook-Air-Veoletta.local>
2026-05-14 21:44:41 +0500

    First task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,42 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr = [];
+  const discriminant = b ** 2 - 4 * a * c;
   
-  return arr;
+  if (discriminant < 0) {
+    return arr;
+  } else if (discriminant === 0) {
+    const root = -b / (2 * a);
+    arr.push(root);
+    return arr;
+  } else {
+    const sqrtD = Math.sqrt(discriminant);
+    const root1 = (-b + sqrtD) / (2 * a);
+    const root2 = (-b - sqrtD) / (2 * a);
+    arr.push(root1, root2);
+    return arr;
+  }
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+  const percentNum = Number(percent);
+  const contributionNum = Number(contribution);
+  const amountNum = Number(amount);
+  const countMonthsNum = Number(countMonths);
   
+  if (isNaN(percentNum) || isNaN(contributionNum) || isNaN(amountNum) || isNaN(countMonthsNum)) {
+    return false;
+  }
+  
+  const creditBody = amountNum - contributionNum;
+  if (creditBody <= 0) {
+    return 0;
+  }
+  
+  const monthlyRate = (percentNum / 100) / 12;
+  const payment = creditBody * (monthlyRate + (monthlyRate / (Math.pow(1 + monthlyRate, countMonthsNum) - 1)));
+  const totalAmount = payment * countMonthsNum;
+  
+  return Number(totalAmount.toFixed(2));
 }
