47b4931fcfbe933a01083c7d36ad9d36aa44ba8c
Веолетта <veoletta@Mac.lan>
2026-06-06 17:57:50 +0500

    Solution for task 3. Arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,12 @@
 function compareArrays(arr1, arr2) {
-  
+  return arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
+  const filteredUsers = users.filter(user => user.gender === gender);
   
+  if (filteredUsers.length === 0) return 0;
+  
+  const totalAge = filteredUsers.reduce((sum, user) => sum + user.age, 0);
+  return totalAge / filteredUsers.length;
 }
